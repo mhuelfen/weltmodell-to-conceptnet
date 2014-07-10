93d0d2e3cceaad5f5dcdeb020a2b5c370989346e
mhuelfen <huelfe@gmx.de>
2014-07-10 23:17:49 +0200

    added logging and finalised code

--- src/wm-cn-conversion.py
+++ src/wm-cn-conversion.py
@@ -6,19 +6,23 @@ Script to convert the weltmodell into the conceptnet.
 @author: Michael A. Huelfenhaus
 '''
 import sys
-import json
 import csv
-import re
 import hashlib
 
 from getopt import getopt, GetoptError
 
-
-
 help_message = '''
-usage: python eval_with_api.py <question_tsv_file> <wikia_name>
+usage: python wm-cn-conversion.py <weltmodell_tsv_file> <result_json_file>
+
+Converts a weltmodell tsv file into the plain conceptnet json format.
+
+Only binary relations are used.
+
+Warning! the json file follows the convention of conceptnet and this
+leads to non valid json, because the single lines are not in a an array.
 '''
 
+# global values for all json items.
 context = '/ctx/all'
 base_concept_url = '/c/en/'
 base_rel_url = '/r/'
@@ -29,25 +33,21 @@ license = 'license'
 
 letters = "abcdefghijklmnopqrstuvwxyz" 
 
-def rand_letters(length):
-    rand_str = ""
-    for i in range(0, length):
-        rand_str += random.sample(letters, 1)[0]
-    return rand_str
-
 def make_id(uri, weight, surface_text):
     '''
     Make a SHA-1 hash of the information that makes this line unique.
     '''
-    
     m = hashlib.sha1()
     m.update(uri)
     m.update(weight)
     m.update(surface_text)
     return m.hexdigest()
 
-def make_json_line(wm_line, concepts):
 
+def make_json_line(wm_line, concepts):
+    '''
+    Transforms an weltmodell line into a conceptnet json line.
+    '''
     # extract realtion
 
     rel = wm_line[1].replace('{___}','').strip().replace(' ','_')
@@ -89,25 +89,32 @@ def make_json_line(wm_line, concepts):
     json_line += '}'
 
     # print wm_line
-    print json_line
+    return json_line
 
 def convert_wm(wm_path, result_path):
-
-    print '['
-     # read wm file
-    with open(wm_path, 'r') as csvfile:
-        reader = csv.reader(csvfile, delimiter='\t')
-        for row in reader:
-            # print row
-            # filter for binary relations
-            concepts = row[4][1:-1].split(', ')
-            #print len(concepts) , row[4]
-            if len(concepts) == 2:
-                # make json line for this wm entry
-                make_json_line(row, concepts)
-                print ','
-                break
-    print ']'
+    '''
+    Converts a weltmodell tsv file into a conceptnet plain json file.
+    '''
+    convert_count = 0
+    # start writing result file
+    with open(result_path, 'w') as result_file:
+         # read wm file
+        with open(wm_path, 'r') as csvfile:
+            reader = csv.reader(csvfile, delimiter='\t')
+            for row in reader:
+                # print row
+                # filter for binary relations
+                concepts = row[4][1:-1].split(', ')
+                #print len(concepts) , row[4]
+                if len(concepts) == 2:
+                    # make json line for this wm entry
+                    json_line =  make_json_line(row, concepts)
+                    #print json_line
+                    result_file.write(json_line + '\n')
+                    convert_count += 1
+                    if convert_count  % 1000 == 0:
+                        print convert_count, 'binary relation lines converted'
+    print 'wm to cn conversion complete: ', convert_count, 'binary relations converted. result written to', result_path 
 
 if __name__ == "__main__":
     '''
